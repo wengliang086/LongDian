bdbe81b3605e5f622381e19225d0df9707d4b405
phoenix <wengliang_1986@126.com>
2017-05-01 12:46:07 +0800

    基础主页面

--- app/src/main/java/com/longdian/activity/MainActivity.java
+++ app/src/main/java/com/longdian/activity/MainActivity.java
@@ -3,14 +3,23 @@ package com.longdian.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.longdian.R;
+import com.longdian.fragment.BaseMsgFragment;
+import com.longdian.fragment.CloudServiceFragment;
+import com.longdian.fragment.EnergyAnalysisFragment;
+import com.longdian.fragment.RealtimeDataFragment;
+import com.longdian.fragment.dummy.DummyContent;
+import com.longdian.util.ToastUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EnergyAnalysisFragment.OnListFragmentInteractionListener {
 
+    private FragmentManager fragmentManager;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
@@ -18,20 +27,23 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.id_energy_analysis:
                     setTitle(R.string.main_navigation_energy_analysis);
+                    replaceFragment(EnergyAnalysisFragment.newInstance(1));
                     return true;
                 case R.id.id_realtime_data:
                     setTitle(R.string.main_navigation_realtime_data);
+                    replaceFragment(new RealtimeDataFragment());
                     return true;
                 case R.id.id_cloud_service:
                     setTitle(R.string.main_navigation_cloud_service);
+                    replaceFragment(new CloudServiceFragment());
                     return true;
                 case R.id.id_base_msg:
                     setTitle(R.string.main_navigation_base_msg);
+                    replaceFragment(new BaseMsgFragment());
                     return true;
             }
             return false;
         }
-
     };
 
     @Override
@@ -39,8 +51,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fragmentManager = getSupportFragmentManager();
+        replaceFragment(EnergyAnalysisFragment.newInstance(1));
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        ToastUtils.showToast(this, item.content);
+    }
 }
---
+++ app/src/main/java/com/longdian/fragment/RealtimeDataFragment.java
@@ -0,0 +1,18 @@
+package com.longdian.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.longdian.R;
+
+public class RealtimeDataFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_realtime_data, container, false);
+    }
+}
